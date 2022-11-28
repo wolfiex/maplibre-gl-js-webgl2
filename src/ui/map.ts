bc9757ff92177b7f61afc3087ecf85657fc0ad6a
Dan Ellis <daniel.ellis.research@gmail.com>
2022-11-28 13:54:58 +0000

    Update map.ts

--- src/ui/map.ts
+++ src/ui/map.ts
@@ -2557,7 +2557,7 @@ class Map extends Camera {
             }
         }, {once: true});
 
-        const gl = this._canvas.getContext('webgl', attributes) ||
+        const gl = this._canvas.getContext('webgl2', attributes) ||
             this._canvas.getContext('experimental-webgl', attributes);
 
         if (!gl) {
